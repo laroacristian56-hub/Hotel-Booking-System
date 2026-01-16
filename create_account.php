e488483b943eedadb45acc53a5a178ebb97ff6e9
Cristian <laroacristian56@gmail.com>
2026-01-16 15:42:58 +0800

    Added Dashboard Page

--- create_account.php
+++ create_account.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="createAccount.css">
+    <link rel="stylesheet" href="css/create-account.css">
+    <link rel="stylesheet" href="css/theme.css">
     <title>Hotel Booking - Create Account</title>
 </head>
 <body>
